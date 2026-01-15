b37a4d46fd4bd4e3b4dcaaf5f9de89cfa49109ae
김대훈 <eogns136@gmail.com>
2026-01-15 16:27:14 +0900

    Feat: Admin 공통 Response 클래스 생성

---
+++ src/main/java/e3i2/ecommerce_backoffice/domain/admin/dto/common/AdminApiResponse2.java
@@ -0,0 +1,20 @@
+package e3i2.ecommerce_backoffice.domain.admin.dto.common;
+
+import lombok.Getter;
+
+@Getter
+public class AdminApiResponse2<T> {
+    private final boolean success;
+    private final String code;
+    private final T data;
+
+    private AdminApiResponse2(boolean success, String code, T data) {
+        this.success = success;
+        this.code = code;
+        this.data = data;
+    }
+
+    public static <T> AdminApiResponse2<T> success(String code, T data) {
+        return new AdminApiResponse2<>(true, code, data);
+    }
+}
