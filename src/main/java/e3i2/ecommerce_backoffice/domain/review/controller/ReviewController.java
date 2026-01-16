3a68bc449f1cd62ac1e9e7f47ae634b0073abdac
Leejun14 <777ljy777@gmail.com>
2026-01-16 12:25:11 +0900

    Chore: feature/R 코드 가져오기

--- src/main/java/e3i2/ecommerce_backoffice/domain/review/controller/ReviewController.java
+++ src/main/java/e3i2/ecommerce_backoffice/domain/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package e3i2.ecommerce_backoffice.domain.review.controller;
 
 import e3i2.ecommerce_backoffice.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
