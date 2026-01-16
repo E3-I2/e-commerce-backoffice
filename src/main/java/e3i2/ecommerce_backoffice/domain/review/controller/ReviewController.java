96ef975328fa2d501d4294ab44fe6677c8cf7853
Lee Hanbi <hanbi6717@naver.com>
2026-01-16 12:20:15 +0900

    Feat: review 엔티티 및 구조 생성

---
+++ src/main/java/e3i2/ecommerce_backoffice/domain/review/controller/ReviewController.java
@@ -0,0 +1,11 @@
+package e3i2.ecommerce_backoffice.domain.review.controller;
+
+import e3i2.ecommerce_backoffice.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+    private final ReviewService reviewService;
+}
